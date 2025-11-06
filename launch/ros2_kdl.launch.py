8cc1187b95eb679384ab146b77570715586ba2a4
Claudia Adamo <cla.adamo@studenti.unina.it>
2025-11-06 12:21:38 +0100

    punto-1-a-con-gazebo

---
+++ launch/ros2_kdl.launch.py
@@ -0,0 +1,66 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.conditions import IfCondition, UnlessCondition
+from launch.event_handlers import OnProcessExit
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+
+def generate_launch_description():
+    # Package path
+    pkg_path = get_package_share_directory('ros2_kdl_package')
+    # Params: containing the robot description
+    params = os.path.join(pkg_path, 'config', 'param.yaml')
+
+    #### Launch ####
+
+    empty_world_launch = IncludeLaunchDescription(
+    PathJoinSubstitution([FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py']),
+    launch_arguments={
+        'pause': 'true',
+        'gz_args': ['-r ', 'empty.sdf'],
+    }.items(),
+    )
+
+
+    iiwa_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [FindPackageShare('iiwa_bringup'), 'launch', 'iiwa.launch.py']),
+         launch_arguments={
+        #     'gui': LaunchConfiguration('gui'),
+             'pause': 'true',
+            'gz_args': ['-r ', 'empty.sdf'],
+        }.items(),
+    )
+
+    # push robot_description to factory and spawn robot in gazebo
+    urdf_spawner_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='urdf_spawner',
+        arguments=['-topic', '/robot_description', '-entity', 'iiwa', '-z', '0.5', '-unpause'],
+        output='screen',
+    )
+
+    #### Nodes ####
+
+    ros2_kdl_node = Node(
+        package='ros2_kdl_package',
+        executable='ros2_kdl_node',
+        output='screen',
+        parameters=[params],
+    )
+
+
+    # List of Arguments and Nodes
+    return LaunchDescription([
+        iiwa_launch,
+        empty_world_launch,
+        ros2_kdl_node,
+        urdf_spawner_node
+    ])
---
+++ src/kdl_control.cpp
@@ -0,0 +1,46 @@
+#include "kdl_control.h"
+
+KDLController::KDLController(KDLRobot &_robot)
+{
+    robot_ = &_robot;
+}
+
+Eigen::VectorXd KDLController::idCntr(KDL::JntArray &_qd,
+                                      KDL::JntArray &_dqd,
+                                      KDL::JntArray &_ddqd,
+                                      double _Kp, double _Kd)
+{
+    // read current state
+    Eigen::VectorXd q = robot_->getJntValues();
+    Eigen::VectorXd dq = robot_->getJntVelocities();
+
+    // calculate errors
+    Eigen::VectorXd e = _qd.data - q;
+    Eigen::VectorXd de = _dqd.data - dq;
+
+    Eigen::VectorXd ddqd = _ddqd.data;
+    return robot_->getJsim() * (ddqd + _Kd*de + _Kp*e)
+            + robot_->getCoriolis() + robot_->getGravity() /*friction compensation?*/;
+}
+
+Eigen::VectorXd KDLController::velocity_control_null(KDL::Frame &_desPos,
+                                      KDL::Twist &_desVel,
+                                      KDL::Twist &_desAcc,
+                                      double _Kpp)
+{
+    // read current state
+    Eigen::VectorXd q = robot_->getJntValues();
+    Eigen::VectorXd dq = robot_->getJntVelocities();
+
+    // calculate errors
+    Eigen::VectorXd e = _qd.data - q;
+    Eigen::VectorXd de = _dqd.data - dq;
+
+    Eigen::VectorXd ddqd = _ddqd.data;
+    return robot_->getJsim()* _Kpp*e_p +(I-Jdag*J)*qdot_0
+            + robot_->getCoriolis() + robot_->getGravity() /*friction compensation?*/;
+
+}
+
+
+
