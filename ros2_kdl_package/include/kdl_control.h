323276c1357e5a9f68297a47898eefbd6e594e5d
Giacomo Ricco <gia.ricco@studenti.unina.it>
2025-11-07 16:38:31 +0100

    HW2:point-1-b-2

---
+++ ros2_kdl_package/include/kdl_control.h
@@ -0,0 +1,32 @@
+#ifndef KDLControl
+#define KDLControl
+
+#include "Eigen/Dense"
+#include "kdl_robot.h"
+#include "utils.h"
+
+class KDLController
+{
+
+public:
+
+    KDLController(KDLRobot &_robot);
+
+    Eigen::VectorXd idCntr(KDL::JntArray &_qd,
+                           KDL::JntArray &_dqd,
+                           KDL::JntArray &_ddqd,
+                           double _Kp,
+                           double _Kd);
+
+    Eigen::VectorXd velocity_control_null(KDL::Frame &_desPos,
+                        //    KDL::Twist &_desVel,
+                        //    KDL::Twist &_desAcc,
+                           double _Kpp);
+
+private:
+
+    KDLRobot* robot_;
+
+};
+
+#endif
